cb714509ddbcb649ac1202f33da5c7220fedfeb4
Travis Dixon <the.trav@gmail.com>
2012-06-09 20:50:26 +1000

    create instead of new

--- app/controllers/loadouts_controller.rb
+++ app/controllers/loadouts_controller.rb
@@ -2,7 +2,7 @@ class LoadoutsController < Resty
   def create
     player = Player.find(params[:loadout][:player_id])
     load = Loadout.find_by_name(params[:loadout][:name])
-    load = Loadout.new(:player_id => player.id, :name => params[:loadout][:name]) if load.nil?
+    load = Loadout.create(:player_id => player.id, :name => params[:loadout][:name]) if load.nil?
 
     load.loadout = load.store_items(player.items)
     load.save!
