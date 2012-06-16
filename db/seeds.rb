d8ea6940fc5abf5beb6108d5980c58003d7f91d9
Travis Dixon <the.trav@gmail.com>
2012-06-16 14:40:58 +1000

    create accounts for phil and trav

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Player.create(:name => "Travis", :email => "the.trav@gmail.com", :password => "Tester1@")
+Player.create(:name => "Phil", :email => "duke.herb@gmail.com", :password => "Tester1@")
