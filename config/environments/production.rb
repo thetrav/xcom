121e7f0b96ab9b186f1c7fc04fd0893b5781c6df
Travis Dixon <the.trav@gmail.com>
2012-06-16 14:48:15 +1000

    compile assets so rails admin works

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Xcom::Application.configure do
   config.assets.initialize_on_precompile = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
